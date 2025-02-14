a4cc0643e5b6e322c918b76ac8d818a8157b46ff
TalipTun <taliptun62@gmail.com>
2025-02-14 22:41:22 +0000

    fixed some designs on navbar

--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -15,40 +15,36 @@ function Navbar() {
           </div>
           <div id="buttons" className="h-96 py-10">
             <ul id="item-container" className="m-0 p-0 h-full flex flex-col justify-between text-center text-[#9a9a9a]">
-                <div className="flex flex-row justify-left pl-6 gap-2">
-                  <img src={home} className="h-10 w-10" />
+                <div className="flex flex-row justify-left pl-8 gap-2 items-center hover:brightness-150 transition duration-300">
+                  <img src={home} className="h-10 w-10 brightness-500" />
                   <li className="text-[20px]">Home</li>
                 </div>
-                <div className="flex flex-row justify-left pl-6 gap-2">
-                  <img src={code} className="h-10 w-10" />
-                  <li className="text-[20px]">Projects</li>
+                <div className="flex flex-row justify-left pl-8 gap-2 brightness-60 items-center hover:brightness-125 transition duration-300">
+                  <img src={code} className="h-10 w-10 invert" />
+                  <li className="text-[20px] brightness-150">Projects</li>
                 </div>
-                <div className="flex flex-row justify-left pl-6 gap-2">
-                  <img src={person} className="h-10 w-10" />
+                <div className="flex flex-row justify-left pl-8 gap-2 items-center hover:brightness-150 transition duration-300">
+                  <img src={person} className="h-10 w-10 brightness-500" />
                   <li className="text-[20px]">About</li>
                 </div>
-                <div className="flex flex-row justify-left pl-6 gap-2">
-                  <img src={skills} className="h-10 w-10" />
-                  <li className="text-[20px]">Skills</li>
+                <div className="flex flex-row justify-left pl-8 gap-2 brightness-70 items-center hover:brightness-125 transition duration-300">
+                  <img src={skills} className="h-10 w-10 invert" />
+                  <li className="text-[20px] brightness-150">Skills</li>
                 </div>
-                <div className="flex flex-row justify-left pl-6 gap-2">
-                  <img src={contact} className="h-10 w-10" />
-                  <li className="text-[20px]">Contact</li>
+                <div className="flex flex-row justify-left pl-8 gap-2 brightness-70 items-center hover:brightness-125 transition duration-300">
+                  <img src={contact} className="h-10 w-10 invert" />
+                  <li className="text-[20px] brightness-150">Contact</li>
                 </div>
               </ul>
           </div>
           <div id="symbols" className="h-15 flex flex-row justify-center gap-3 items-center">
-            <img src={icon} className="h-10 w-10" />
-            <img src={icon} className="h-10 w-10" />
-            <img src={icon} className="h-10 w-10" />
+            <img src={icon} className="h-10 w-10 transition duration-300 hover:brightness-125" />
+            <img src={icon} className="h-10 w-10 transition duration-300 hover:brightness-125" />
+            <img src={icon} className="h-10 w-10 transition duration-300 hover:brightness-500" />
           </div>
         </div>
       </>
     )
   }
   
-  export default Navbar
-
-  // color: #2c3e50; text color
-  // color: black 
-  // color: background: #181818;
+  export default Navbar;
