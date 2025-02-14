de0d9164dbb4536695a2806ed3789044731f1534
TalipTun <taliptun62@gmail.com>
2025-02-14 17:05:31 +0000

    change navbar design

--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -2,38 +2,38 @@ import icon from "../icons/icon.png" ;
 import code from "../icons/code.png"; 
 import home from "../icons/home.png"; 
 import contact from "../icons/contact.png"; 
-import person from "../icons/code.png"; 
+import person from "../icons/person.png"; 
 import skills from "../icons/skills.png"; 
 
 function Navbar() {
     return (
       <>
-        <div id="container" className="bg-amber-400 flex flex-col w-48 fixed left-0 top-0 bottom-0">
-          <div id="topbit" className="h-56 bg-amber-800">
+        <div id="container" className="bg-[#181818] flex flex-col w-48 fixed left-0 top-0 bottom-0">
+          <div id="topbit" className="h-56 bg-black">
             <p className="text-4xl h-40 flex justify-center items-center text-white">Talip Tun</p>
-            <p className="text-xl h-16 flex justify-center items-center text-white">CS undergraduate</p>
+            <p className="text-xl h-16 flex justify-center items-center text-[#9a9a9a]">CS undergraduate</p>
           </div>
           <div id="buttons" className="h-96 py-10">
-            <ul id="item-container" className="m-0 p-0 h-full flex flex-col justify-between text-center text-white">
+            <ul id="item-container" className="m-0 p-0 h-full flex flex-col justify-between text-center text-[#9a9a9a]">
                 <div className="flex flex-row justify-left pl-6 gap-2">
                   <img src={home} className="h-10 w-10" />
-                  <li className="text-2xl">Home</li>
+                  <li className="text-[20px]">Home</li>
                 </div>
                 <div className="flex flex-row justify-left pl-6 gap-2">
                   <img src={code} className="h-10 w-10" />
-                  <li className="text-2xl">Projects</li>
+                  <li className="text-[20px]">Projects</li>
                 </div>
                 <div className="flex flex-row justify-left pl-6 gap-2">
                   <img src={person} className="h-10 w-10" />
-                  <li className="text-2xl">About</li>
+                  <li className="text-[20px]">About</li>
                 </div>
                 <div className="flex flex-row justify-left pl-6 gap-2">
                   <img src={skills} className="h-10 w-10" />
-                  <li className="text-2xl">Skills</li>
+                  <li className="text-[20px]">Skills</li>
                 </div>
                 <div className="flex flex-row justify-left pl-6 gap-2">
                   <img src={contact} className="h-10 w-10" />
-                  <li className="text-2xl">Contact</li>
+                  <li className="text-[20px]">Contact</li>
                 </div>
               </ul>
           </div>
@@ -48,3 +48,7 @@ function Navbar() {
   }
   
   export default Navbar
+
+  // color: #2c3e50; text color
+  // color: black 
+  // color: background: #181818;
--- portfolio/src/pages/home.tsx
+++ portfolio/src/pages/home.tsx
@@ -1,10 +1,22 @@
 import "tailwindcss";
 import Navbar from "../components/navbar.tsx"
+import photo from "../assets/image.jpg"
 function Home() {
     return (
       <>
-      <div className="h-3000 bg-black">
+      <div className="h-screen w-screen pl-48 bg-black">
         <Navbar />
+        <div>
+          <div id="project1" className="group relative w-80 h-80 bg-amber-50 flex flex-col justify-center items-center">
+            <p>Http Server with C</p>
+            <img src={photo} alt="Project Image" />
+
+            {/* Hidden description */}
+            <div className="absolute inset-0 bg-black bg-opacity-80 text-red-500 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <p className="text-center">This is a simple HTTP server implemented in C using sockets.</p>
+            </div>
+          </div>
+        </div>
       </div>
       </>
     )
